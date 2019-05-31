feb785bd7b955e11757cf77a4050ec4330d988a5
Anna Szmyga <anna.szmyga@o2.pl>
2019-05-31 12:39:39 +0200

    Add reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './containers/App';
+import store from './store/index';
+import DevTools from './devTools';
+import {getCountries} from './actions/actions.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <>
+            <App />
+            <DevTools />
+        </>
+    </Provider>, 
+    document.getElementById('root')
+);
+
+store.dispatch(getCountries());
 
 
